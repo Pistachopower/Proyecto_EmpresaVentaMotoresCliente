1d5b7668610e0ceee90c5fcbd54a9837fc1c0778
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-01-31 14:39:37 +0100

    se soluciona error de tokens

--- EmpresaVentaPiezasCoche_cliente/views.py
+++ EmpresaVentaPiezasCoche_cliente/views.py
@@ -10,11 +10,19 @@ def index(request):
 
 
 def empleados_lista_api(request):
-
-    response= requests.get('http://127.0.0.1:8080/api/v1/empleados')
-    
+    headers= {'Authorization': 'Bearer AqWr2niC9doTnKRmpELEixGbutYxO3'} 
+    response= requests.get('http://127.0.0.1:8080/api/v1/empleados', headers=headers)
     empleados= response.json()
-    
     return render(request, 'empleado/lista.html', {'empleados': empleados})
 
 
+#def empleados_lista_api(request):
+#    # obtenemos todos los libros
+#    headers = {'Authorization': 'Bearer '+ env('token')} 
+#    headers = {'Authorization': 'Bearer '+request.session["token"]} 
+#    print(headers)
+#    response = requests.get('http://127.0.0.1:8000/api/v1/libros',headers=headers)
+#   # Transformamos la respuesta en json
+#    libros = response.json()
+#    #return render(request, 'libro/lista.html',{"libros_mostrar":libros})
+#    return render(request, 'libro/lista_mejorada.html',{"libros_mostrar":libros})
