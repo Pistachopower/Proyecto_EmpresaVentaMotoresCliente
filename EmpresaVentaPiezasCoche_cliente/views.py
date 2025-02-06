c8c9c4ec5eb3ca5d3b270d7743c636ecc1fa118a
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-02-06 12:04:09 +0100

    otro cambio

--- EmpresaVentaPiezasCoche_cliente/views.py
+++ EmpresaVentaPiezasCoche_cliente/views.py
@@ -32,14 +32,14 @@ def empleados_lista_api_mejorado(request):
     return render(request, 'empleado/lista_Mejorado.html', {'empleados': empleados})
 
 def listar_clientes_mejorado(request):
-    headers= {'Authorization': 'Bearer bRRnYZbUj4pzm52c9tdl3Uc7tjBZoQ'} 
+    headers= {'Authorization': 'Bearer 0eRLQm0yVN3346rkzVB01b4MX2tvC8'} 
     response= requests.get('http://127.0.0.1:8080/api/v1/clientes_mejorado', headers=headers)
     cliente= response.json()
     return render(request, 'cliente/lista_Mejorado.html', {'cliente_mejorado': cliente})
 
 
 def listar_pedido_mejorado(request):
-    headers= {'Authorization': 'Bearer bRRnYZbUj4pzm52c9tdl3Uc7tjBZoQ'} 
+    headers= {'Authorization': 'Bearer 0eRLQm0yVN3346rkzVB01b4MX2tvC8'} 
     response= requests.get('http://127.0.0.1:8080/api/v1/pedido_mejorado', headers=headers)
     pedido= response.json()
     return render(request, 'pedido/lista_Mejorado.html', {'pedido_mejorado': pedido})
